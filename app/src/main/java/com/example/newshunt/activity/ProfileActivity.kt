0ceeccb8c67baf9a5f8ff1ec1b8690095891dec3
SulemanAfzal <sulemanafzalbkuc1998@gmail.com>
2023-08-04 22:32:32 +0500

    Update news detail item show in news fragmen using coolapsing

--- app/src/main/java/com/example/newshunt/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/newshunt/activity/ProfileActivity.kt
@@ -1,12 +1,74 @@
 package com.example.newshunt.activity
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.newshunt.R
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.TextView
+import com.example.newshunt.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.etUserName.isEnabled = false
+        binding.etUseremial.isEnabled = false
+        binding.etUsernumber.isEnabled = false
+        binding.etUserpassword.isEnabled = false
+
+        binding.tvUserName.setOnClickListener {
+            enableEditTextAndShowCursor(
+                binding.etUserName,
+                binding.tvUserName
+            )
+        }
+
+        binding.tvUseremial.setOnClickListener {
+            enableEditTextAndShowCursor(
+                binding.etUseremial,
+                binding.tvUseremial
+            )
+        }
+
+        binding.tvUserpassword.setOnClickListener {
+            enableEditTextAndShowCursor(
+                binding.etUserpassword,
+                binding.tvUserpassword
+            )
+        }
+
+        binding.tvUsernumber.setOnClickListener {
+            enableEditTextAndShowCursor(
+                binding.etUsernumber,
+                binding.tvUsernumber
+            )
+        }
+
+        binding.btnSave.setOnClickListener {
+            // Show the "Edit" TextViews again
+            binding.tvUserName.visibility = View.VISIBLE
+            binding.tvUseremial.visibility = View.VISIBLE
+            binding.tvUserpassword.visibility = View.VISIBLE
+            binding.tvUsernumber.visibility = View.VISIBLE
+        }
+    }
+
+    private fun enableEditTextAndShowCursor(editText: EditText, textView: TextView) {
+        editText.isEnabled = true
+        editText.isFocusableInTouchMode = true
+        editText.requestFocus()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+
+        editText.setSelection(editText.text.length)
+        textView.visibility = View.GONE
     }
 }
--- app/src/main/java/com/example/newshunt/fragment/NewsFragment.kt
+++ app/src/main/java/com/example/newshunt/fragment/NewsFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.newshunt.R
 import com.example.newshunt.adapter.ViewPagerAdapter
 import com.example.newshunt.databinding.FragmentNewsBinding
+import com.google.android.material.appbar.AppBarLayout
 
 class NewsFragment : Fragment() {
     private lateinit var binding: FragmentNewsBinding
@@ -21,7 +24,7 @@ class NewsFragment : Fragment() {
         val adapter = ViewPagerAdapter(childFragmentManager) // Use childFragmentManager here
 
 
-        binding.viewPager.adapter = adapter
+        /*  binding.viewPager.adapter = adapter
 
         binding.tabLayout.setupWithViewPager(binding.viewPager)
 
@@ -29,8 +32,34 @@ class NewsFragment : Fragment() {
         binding.tabLayout.getTabAt(0)?.text = "All News"
         binding.tabLayout.getTabAt(1)?.text = "Sports"
         binding.tabLayout.getTabAt(2)?.text = "Technology"
-        binding.tabLayout.getTabAt(3)?.text = "Politics"
+        binding.tabLayout.getTabAt(3)?.text = "Politics"*/
         // Set click listener for the hamburger_icon
         return binding.root // Return the root view of the binding
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val collapsingToolbarLayout = binding.collapsingToolbarLayout
+        val appBarLayout = binding.appBarLayout
+        val toolbarImageView = binding.toolbarbackImage
+        val toolbarTextView = binding.toolbarTV
+
+        // Set the initial toolbar icon and text color here if needed
+        toolbarImageView.setImageResource(R.drawable.back_icon_white)
+        toolbarTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+
+        appBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val maxScroll = appBarLayout.totalScrollRange
+            val percentage = Math.abs(verticalOffset).toFloat() / maxScroll.toFloat()
+
+            val iconRes =
+                if (percentage >= 0.8) R.drawable.back_icon else R.drawable.back_icon_white
+            val textColorRes = if (percentage >= 0.8) R.color.black else R.color.white
+
+            toolbarImageView.setImageResource(iconRes)
+            toolbarTextView.setTextColor(ContextCompat.getColor(requireContext(), textColorRes))
+        })
+
     }
 }
